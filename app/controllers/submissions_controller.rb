9c16aade0b397c4ea558c3584f26adb15fd74025
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-12-03 17:53:25 +0000

    Added submission routes

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,5 +1,19 @@
 class SubmissionsController < ApplicationController
+  
+  def home
+    render({ :template => "submissions/home" })
+  end
+
+  def write
+    render({ :template => "submissions/write" })
+  end
+
   def index
     render({ :template => "submissions/index" })
   end
+
+  def show
+    render({ :template => "submissions/show" })
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get("/", { :controller => "submissions", :action => "index" })
+  get("/", { :controller => "submissions", :action => "home" })
+  get("/write", { :controller => "submissions", :action => "write" })
+  get("/submissions", { :controller => "submissions", :action => "index" })
+  get("/submissions/:submission_id", { :controller => "submissions", :action => "show" })
 
   # Defines the root path route ("/")
   # root "articles#index"
